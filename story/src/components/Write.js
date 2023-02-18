8f796c23f1afc927e2c3f88599670a09232aa671
Zayn Abou-Harb <zayn.abou-harb@wsu.edu>
2023-02-18 14:40:52 -0800

    Added text input to write page

--- story/src/components/Write.js
+++ story/src/components/Write.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router";
+
+import Page from "./Page";
 
 const Write = () => {
+    const navigate = useNavigate();
+
+    const [branchText, setBranchText] = useState('')
+    const [pageText, setPageText] = useState('')
+
+    const addPage = async(e) => {
+        e.preventDefault();
+
+        console.log("ADD INFO TO DATABASE")
+
+        navigate('/')
+    }
+
     return (
-        <div>This is the Write Page!</div>
+        <div>
+            <h2>This is the Write Page!</h2>
+            <form onSubmit={addPage}>
+                <label>Branch:</label>
+                <textarea value={branchText} onChange={(e) => setBranchText(e.target.value)} />
+                <br />
+
+                <label>Please Continue the story:</label>
+                <textarea value={pageText} onChange={(e) => setPageText(e.target.value)} />
+                <br />
+
+
+
+
+                <button type="submit">Add Page</button>
+            </form>
+        </div>
     )
 }
 
