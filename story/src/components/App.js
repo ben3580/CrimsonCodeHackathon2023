468f00cff42e0cd1def9638d164899c41914dbe0
aydenarmst <ayden.armstrong@wsu.edu>
2023-02-18 14:51:18 -0800

    Adding Auth0 authentication.

--- story/src/components/App.js
+++ story/src/components/App.js
@@ -6,9 +6,22 @@ import Landing from './Landing'
 import Read from './Read'
 import Write from './Write'
 
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
+
 const App = () => {
+  const {isLoading, error} = useAuth0();
   return (
     <div className=''>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+        </>
+      )}
         <BrowserRouter>
             <Header />
             <Routes>
--- story/src/index.js
+++ story/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-    <App name="App"/>
+    <Auth0Provider
+        domain= "dev-2z0ys20vkpn2djq1.us.auth0.com"
+        clientId= "J7kGu9UbOVGVKzNROKPuvqCl036xKW1o"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <App name="App"/>
+    </Auth0Provider>
 );
