6f85ac519bda9821f31d86a9fd68173478be5382
Ben <shuyan.liu1@wsu.edu>
2023-02-18 17:38:05 -0800

    Remove earlier work

--- ben_html/index.js
+++ ben_html/index.js
@@ -1,17 +1,16 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const { Configuration, OpenAIApi } = require("openai");
 
-// Get the header
-var header = document.getElementById("myHeader");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
+  temperature: 0.7,
+  max_tokens: 64,
+  top_p: 1.0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+});
