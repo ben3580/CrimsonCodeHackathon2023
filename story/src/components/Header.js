4a7c1e728bbad7859f6e9ca34ab2f51fcceb38d0
Ben <shuyan.liu1@wsu.edu>
2023-02-18 16:14:32 -0800

    Move stuff over to React App

--- story/src/components/Header.js
+++ story/src/components/Header.js
@@ -2,22 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import HB from "../images/home_button.png"
+
 const Header = (props) => {
     const { logout, isAuthenticated } = useAuth0();
 
     return (
-        <div>
-            <Link to="/">
-                Home
-            </Link>
-            <Link to="/Read">
-                Read
-            </Link>
-            { isAuthenticated ?
-                <Link to="/Write">
-                    Write
-                </Link>
-            : <></>}
+        <div className="header">
+            <div className="row dark_theme">
+                <div className="col-1 center dark_theme">
+                    <img src={HB} alt="Home button" width="30px" height="30px" />
+                </div>
+                <div className="col-9 dark_theme">
+                    <h3>Title</h3>
+                </div>
+                <div className="col-2 center dark_theme">
+                    <a className="btn btn-success" href="#" role="button">Sign up / Login</a>
+                </div>
+            </div>
         </div>
     );
 }
